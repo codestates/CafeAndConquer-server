82d93c32afa81c017d5f06d63351e2fb0950fe8f
LeeSuHo <angelxtry@gmail.com>
2019-10-19 17:06:52 +0900

    Create DB Table
    Make express index.js
    Create User, Cafe Table

---
+++ models/cafe.js
@@ -0,0 +1,50 @@
+module.exports = (sequelize, DataTypes) => {
+  const Cafe = sequelize.define(
+    'cafe',
+    {
+      cafeName: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING(250),
+        allowNull: false,
+      },
+      point: {
+        type: DataTypes.GEOMETRY('POINT'),
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      open24hour: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      priceIceAmericano: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: true,
+      },
+      enoughOutlets: {
+        type: DataTypes.ENUM('LACK', 'NORMAL', 'MANY'),
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+      freezeTableName: true,
+    },
+  );
+
+  Cafe.associate = (db) => {
+    db.Cafe.belongsTo(db.User, {
+      foreignKey: { name: 'pioneer', allowNull: false },
+      targetKey: 'id',
+    });
+  };
+
+  return Cafe;
+};
---
+++ models/user.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+      },
+      nick: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+      provider: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        defaultValue: 'local',
+      },
+      snsId: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+    },
+  );
+
+  User.associate = (db) => {
+    db.User.hasMany(db.Cafe, {
+      foreignKey: { name: 'pioneer', allowNull: false },
+      sourceKey: 'id',
+    });
+  };
+
+  return User;
+};
